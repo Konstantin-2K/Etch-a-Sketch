67a94a550dadef87d86d30a7a61614a7611e67d3
Konstantin-2K <koseto2k@gmail.com>
2023-03-08 23:33:40 +0200

    Nearly finished

--- script.js
+++ script.js
@@ -1,44 +1,67 @@
 let container = document.querySelector('.container');
 
 let slider = document.querySelector('.slider');
-slider.addEventListener('mouseup', getNewBlockSize);
+slider.addEventListener('input', getNewBlockSize);
 
 let blockSize = (600 / 16).toString() + 'px';
+
+let paintMode = '';
 
 let clearBtn = document.querySelector('.clear');
 clearBtn.addEventListener('click', clearCanvas);
 
 let eraserBtn = document.querySelector('.eraser');
-eraserBtn.addEventListener('click', erase);
+eraserBtn.onclick = () => paintMode = 'erase';
+
+let colorBtn = document.querySelector('.color');
+colorBtn.onclick = () => paintMode = 'color';
+
+let rainbowBtn = document.querySelector('.rainbow');
+rainbowBtn.onclick = () => paintMode = 'rainbow';
+
+let count = document.querySelector('.count');
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 function drawBase(blockNum) {
-    for(let i = 0; i < blockNum * blockNum; i++) {
+    for (let i = 0; i < blockNum * blockNum; i++) {
         let block = document.createElement('div');
-        
+
         block.style.width = blockSize;
         block.style.height = blockSize;
         block.classList.add('block');
         container.appendChild(block);
-        addListeners();
     }
+    addListeners();
 }
 
 function addListeners() {
     let blocks = document.querySelectorAll('.block');
-    for(let block of blocks) {
-        block.addEventListener('mouseover', changeColor);
+    for (let block of blocks) {
+        block.addEventListener('mouseover', changeColor)
+        block.addEventListener('mousedown', changeColor)
     }
 }
 
-function changeColor() {
-    this.style.cssText += 'background: purple;';
-}
-
-function erase() {
-    block.style.cssText += 'background: purple;';
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) {
+        return
+    } else if (paintMode === 'color') {
+        this.style.cssText += 'background: purple;';
+    } else if (paintMode === 'rainbow') {
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)
+        this.style.cssText += `rgb(${randomR}, ${randomG}, ${randomB})`;
+    } else if (paintMode === 'erase') {
+        this.style.cssText += 'background: white;';
+    }
 }
 
 function getNewBlockSize() {
+    count.textContent = slider.value + ' X ' + slider.value;
     blockSize = (600 / slider.value).toString() + 'px';
     console.log("New block size: " + blockSize);
     container.innerHTML = '';
@@ -47,7 +70,7 @@ function getNewBlockSize() {
 
 function clearCanvas() {
     blocks = document.querySelectorAll('.block');
-    for(let block of blocks) {
+    for (let block of blocks) {
         block.style.cssText += 'background: white;'
     }
 }
